2f70a3b5809263d94ed313ed933d2d7d8a3f6c42
Thiago <thiagojustinianosilva@gmail.com>
2025-05-28 16:11:26 -0300

    Implementação de Persistência de Dados no Aplicativo em Desenvolvimento v2

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -2,13 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBatzmk9gxYv3B_0txg21Bqw27W9OfrXDI",
-  authDomain: "quero-um-pet-c48d8.firebaseapp.com",
-  projectId: "quero-um-pet-c48d8",
-  storageBucket: "quero-um-pet-c48d8.appspot.com",
-  messagingSenderId: "773991145084",
-  appId: "1:773991145084:web:d56a83ca7a4a6ed833f3f9",
-  measurementId: "G-VDEVR7E8DX"
+
 };
 
 const app = initializeApp(firebaseConfig);
