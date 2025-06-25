4925315563d69a72a4667440552e51087eafb684
Thiago <thiagojustinianosilva@gmail.com>
2025-06-25 19:08:38 -0300

    manual de usuario

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -7,13 +7,12 @@ import {
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBatzmk9gxYv3B_0txg21Bqw27W9OfrXDI",
-  authDomain: "quero-um-pet-c48d8.firebaseapp.com",
-  projectId: "quero-um-pet-c48d8",
-  storageBucket: "quero-um-pet-c48d8.appspot.com",
-  messagingSenderId: "773991145084",
-  appId: "1:773991145084:web:d56a83ca7a4a6ed833f3f9",
-  measurementId: "G-VDEVR7E8DX"
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_AUTH_DOMAIN",
+  projectId: "SEU_PROJECT_ID",
+  storageBucket: "SEU_BUCKET",
+  messagingSenderId: "SEU_SENDER_ID",
+  appId: "SEU_APP_ID",
 };
 
 const app = initializeApp(firebaseConfig);
